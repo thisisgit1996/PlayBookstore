4bf6e0f71f9f703a13f804a12ae57998300c9ca9
Leo <leo.baker@qa.com>
2018-01-29 17:39:24 +0000

    Downloaded bootstrap and started creating website

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,11 @@ import play.api.mvc._
 class Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Hello World"))
+    Ok(views.html.index())
+  }
+
+  def navbar = Action {
+    Ok(views.html.navbar())
   }
 
 }
